2c39fb79c2a7e15659c9f29a59d9e5a974b227f3
FabianaArciniegas <144750464+FabianaArciniegas@users.noreply.github.com>
2023-09-30 14:50:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+/* Ejercicio: hacer una función que recorra un array de nombres de
+personas y los imprima en el html a media que vaya recorriendo
+el array */
+
+
+var nombres = ["andres", "juan", "marcos", "matias", "ricardo"];
+
+function mostrarnombres (nombres) {
+   for (let i=0; i<nombres.length; i++){
+      document.write(nombres[i] + " ");
+  }
+}
+
+mostrarnombres(nombres);
 
